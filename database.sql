0216e9c55c93ecd252c9f7d1531324a5c81399d6
Kachun-QLJG <1049875596@qq.com>
2021-11-30 04:25:02 +0000

    删除文件 测试.txt

---
+++ database.sql
@@ -0,0 +1,205 @@
+﻿# Host: 127.0.0.1  (Version: 5.7.26)
+# Date: 2021-11-30 19:54:53
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "arrangement"
+#
+
+DROP TABLE IF EXISTS `arrangement`;
+CREATE TABLE `arrangement` (
+  `order_number` varchar(11) NOT NULL,
+  `project_number` varchar(8) NOT NULL,
+  `repairman_number` varchar(8) NOT NULL,
+  `parts_number` varchar(8) NOT NULL,
+  `parts_count` int(2) NOT NULL,
+  `progress` varchar(6) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "arrangement"
+#
+
+/*!40000 ALTER TABLE `arrangement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `arrangement` ENABLE KEYS */;
+
+#
+# Structure for table "attorney"
+#
+
+DROP TABLE IF EXISTS `attorney`;
+CREATE TABLE `attorney` (
+  `number` varchar(11) NOT NULL,
+  `user_id` varchar(8) NOT NULL,
+  `vehicle_number` varchar(17) NOT NULL,
+  `repair_type` varchar(4) DEFAULT NULL,
+  `classification` varchar(4) DEFAULT NULL,
+  `pay_method` varchar(4) DEFAULT NULL,
+  `start_time` varchar(20) NOT NULL,
+  `salesman_id` varchar(8) DEFAULT NULL,
+  `predict_finish_time` varchar(20) DEFAULT NULL,
+  `actual_finish_time` varchar(20) DEFAULT NULL,
+  `rough_problem` varchar(255) NOT NULL,
+  `specific_problem` varchar(255) NOT NULL,
+  `progress` varchar(10) NOT NULL,
+  `total_cost` double(6,2) NOT NULL,
+  PRIMARY KEY (`number`),
+  UNIQUE KEY `uix_attorney_number` (`number`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "attorney"
+#
+
+/*!40000 ALTER TABLE `attorney` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attorney` ENABLE KEYS */;
+
+#
+# Structure for table "auth_session"
+#
+
+DROP TABLE IF EXISTS `auth_session`;
+CREATE TABLE `auth_session` (
+  `time_hash` varchar(64) NOT NULL,
+  `last_visit` varchar(30) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  PRIMARY KEY (`time_hash`),
+  UNIQUE KEY `uix_auth_session_time_hash` (`time_hash`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "auth_session"
+#
+
+/*!40000 ALTER TABLE `auth_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_session` ENABLE KEYS */;
+
+#
+# Structure for table "parts_overview"
+#
+
+DROP TABLE IF EXISTS `parts_overview`;
+CREATE TABLE `parts_overview` (
+  `parts_number` varchar(8) NOT NULL,
+  `parts_name` varchar(50) NOT NULL,
+  `parts_cost` double(8,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "parts_overview"
+#
+
+/*!40000 ALTER TABLE `parts_overview` DISABLE KEYS */;
+/*!40000 ALTER TABLE `parts_overview` ENABLE KEYS */;
+
+#
+# Structure for table "repairman"
+#
+
+DROP TABLE IF EXISTS `repairman`;
+CREATE TABLE `repairman` (
+  `number` varchar(8) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `current_work_hour` int(4) NOT NULL,
+  PRIMARY KEY (`number`),
+  UNIQUE KEY `uix_repairman_number` (`number`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "repairman"
+#
+
+/*!40000 ALTER TABLE `repairman` DISABLE KEYS */;
+INSERT INTO `repairman` VALUES ('RE-0246','奚嘉骏','$2a$10$6NBVE248Ig9/TFz9QW9vquTsTH.HLlK2y8M.z4D/FaYA8wsnAnePq','机修工',0),('RE-0248','伍慕庭','$2a$10$pfPLL4MczpKHpmaALWSbCO90wBz47v/Ce6ssN6YUAMVPNMWUsgHVe','电工',0),('RE-0249','夏逸凡','$2a$10$XtptbBnCexoEUmAzLV1TCeqrJTepnKJZsiMgtpYNd76a9/ti6ti.m','钣金工',0),('RE-0251','张霖锋','$2a$10$U159VfMPR9IzZpxlCvdLH.DbUL5Rr.ufYfReUDpWrWlV3wU57Nibi','喷漆工',0);
+/*!40000 ALTER TABLE `repairman` ENABLE KEYS */;
+
+#
+# Structure for table "salesman"
+#
+
+DROP TABLE IF EXISTS `salesman`;
+CREATE TABLE `salesman` (
+  `number` varchar(8) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`number`),
+  UNIQUE KEY `uix_salesman_number` (`number`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "salesman"
+#
+
+/*!40000 ALTER TABLE `salesman` DISABLE KEYS */;
+INSERT INTO `salesman` VALUES ('SA-0246','奚嘉骏','$2a$10$twmzVNJP1m09vTIysSB8K.939/zQ666zv.w96ox2nbr70hqys6NqW'),('SA-0248','伍慕庭','$2a$10$ZUnlWs99.r2rT4vXekXLru4hRRYNVcBKZ/EOs/o.8NoCRh1muE9jq'),('SA-0249','夏逸凡','$2a$10$RNyBPg7160koaffiF.DdIO90b5Fzf1zIcWN8qLvl.PWjsjwjatJrK'),('SA-0251','张霖锋','$2a$10$dsp2i6FCvYzlFvU8pD8R7O/nj/ok5I/R.qSfKlBrrA.rbeQomQOvu');
+/*!40000 ALTER TABLE `salesman` ENABLE KEYS */;
+
+#
+# Structure for table "type_overview"
+#
+
+DROP TABLE IF EXISTS `type_overview`;
+CREATE TABLE `type_overview` (
+  `project_number` varchar(8) NOT NULL,
+  `project_name` varchar(100) NOT NULL,
+  `work_hour` int(3) NOT NULL,
+  `type` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "type_overview"
+#
+
+/*!40000 ALTER TABLE `type_overview` DISABLE KEYS */;
+/*!40000 ALTER TABLE `type_overview` ENABLE KEYS */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `number` varchar(8) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `property` varchar(4) DEFAULT NULL,
+  `discount_rate` int(2) DEFAULT NULL,
+  `contact_person` varchar(10) DEFAULT NULL,
+  `contact_tel` varchar(20) NOT NULL,
+  PRIMARY KEY (`number`),
+  UNIQUE KEY `uix_user_number` (`number`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+#
+# Structure for table "vehicle"
+#
+
+DROP TABLE IF EXISTS `vehicle`;
+CREATE TABLE `vehicle` (
+  `number` varchar(17) NOT NULL,
+  `license_number` varchar(10) NOT NULL,
+  `user_id` varchar(8) NOT NULL,
+  `color` varchar(10) NOT NULL,
+  `model` varchar(40) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  PRIMARY KEY (`number`),
+  UNIQUE KEY `uix_vehicle_number` (`number`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "vehicle"
+#
+
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
---
+++ server/server.go
@@ -0,0 +1,449 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"github.com/dchest/captcha"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type User struct { //客户表
+	Number        string `gorm:"type:varchar(8);unique_index;not null;primary_key"` //客户编号
+	Password      string `gorm:"type:varchar(255);not null"`                        //密码
+	Name          string `gorm:"type:varchar(100)"`                                 //姓名
+	Property      string `gorm:"type:varchar(4)"`                                   //性质
+	DiscountRate  int    `gorm:"type:int(2)"`                                       //折扣率
+	ContactPerson string `gorm:"type:varchar(10)"`                                  //联系人
+	ContactTel    string `gorm:"type:varchar(20);not null;unique_index"`            //联系电话
+}
+
+type Salesman struct { //业务员表
+	Number   string `gorm:"type:varchar(8);unique_index;not null;primary_key"` //工号
+	Name     string `gorm:"type:varchar(20);not null"`                         //姓名
+	Password string `gorm:"type:varchar(255);not null"`                        //密码
+}
+
+type Repairman struct { //维修员表
+	Number          string `gorm:"type:varchar(8);unique_index;not null;primary_key"` //工号
+	Name            string `gorm:"type:varchar(20);not null"`                         //姓名
+	Password        string `gorm:"type:varchar(255);not null"`                        //密码
+	Type            string `gorm:"type:varchar(10);not null"`                         //工种
+	CurrentWorkHour int    `gorm:"type:int(4);not null"`                              //当前工时
+}
+
+type TypeOverview struct { //工种总览表
+	ProjectNumber string `gorm:"type:varchar(8);not null"`   //维修项目编号
+	ProjectName   string `gorm:"type:varchar(100);not null"` //维修项目名称
+	WorkHour      string `gorm:"type:int(3);not null"`       //工时
+	Type          string `gorm:"type:varchar(10);not null"`  //工种
+}
+
+type PartsOverview struct { //零件总览表
+	PartsNumber string  `gorm:"type:varchar(8);not null"`  //零件编号
+	PartsName   string  `gorm:"type:varchar(50);not null"` //零件名称
+	PartsCost   float64 `gorm:"type:double(8,2);not null"` //零件价格
+}
+
+type Vehicle struct { //车辆表
+	Number        string `gorm:"type:varchar(17);not null;unique_index;primary_key"` //车架号
+	LicenseNumber string `gorm:"type:varchar(10);not null"`                          //最后一次访问的时间戳（精确到秒）
+	UserLink      User   `gorm:"ForeignKey:UserID"`                                  //外键，用户表
+	UserID        string `gorm:"type:varchar(8);not null"`                           //客户编号
+	Color         string `gorm:"type:varchar(10);not null"`                          //车辆颜色
+	Model         string `gorm:"type:varchar(40);not null"`                          //车型
+	Type          string `gorm:"type:varchar(10);not null"`                          //车型
+}
+
+type Attorney struct { //委托书表
+	Number            string   `gorm:"type:varchar(11);not null;unique_index;primary_key"` //订单号
+	UserLink          User     `gorm:"ForeignKey:UserID"`                                  //外键，用户表
+	UserID            string   `gorm:"type:varchar(8);not null"`                           //客户编号
+	VehicleLink       Vehicle  `gorm:"ForeignKey:VehicleNumber"`                           //外键，车辆表
+	VehicleNumber     string   `gorm:"type:varchar(17);not null"`                          //车架号
+	RepairType        string   `gorm:"type:varchar(4)"`                                    //维修类型
+	Classification    string   `gorm:"type:varchar(4)"`                                    //作业分类
+	PayMethod         string   `gorm:"type:varchar(4)"`                                    //结算方式
+	StartTime         string   `gorm:"type:varchar(20);not null"`                          //进场时间
+	SalesmanLink      Salesman `gorm:"ForeignKey:SalesmanID"`                              //外键，业务员表
+	SalesmanID        string   `gorm:"type:varchar(8)"`                                    //业务员编号
+	PredictFinishTime string   `gorm:"type:varchar(20)"`                                   //预计完工时间时间
+	ActualFinishTime  string   `gorm:"type:varchar(20)"`                                   //实际完工时间时间
+	RoughProblem      string   `gorm:"type:varchar(255);not null"`                         //粗略故障描述
+	SpecificProblem   string   `gorm:"type:varchar(255);not null"`                         //详细故障描述
+	Progress          string   `gorm:"type:varchar(10);not null"`                          //进展
+	TotalCost         float64  `gorm:"type:double(6,2);not null"`                          //总价
+}
+
+type Arrangement struct { //派工单表
+	AttorneyLink    Attorney      `gorm:"ForeignKey:OrderNumber"`     //外键，委托书表
+	OrderNumber     string        `gorm:"type:varchar(11);not null"`  //订单号
+	ProjectNumber   string        `gorm:"type:varchar(8);not null"`   //维修项目编号
+	RepairmanLink   Repairman     `gorm:"ForeignKey:RepairmanNumber"` //外键，委托书表
+	RepairmanNumber string        `gorm:"type:varchar(8);not null"`   //订单号
+	PartsLink       PartsOverview `gorm:"ForeignKey:PartsNumber"`     //外键，零件总览表
+	PartsNumber     string        `gorm:"type:varchar(8);not null"`   //零件号
+	PartsCount      int           `gorm:"type:int(2);not null"`       //零件数量
+	Progress        string        `gorm:"type:varchar(6);not null"`   //进展
+}
+
+type AuthSession struct { //登录表
+	TimeHash  string `gorm:"type:varchar(64);not null;unique_index;primary_key"` //时间戳的哈希值
+	LastVisit string `gorm:"type:varchar(30);not null"`                          //最后一次访问的时间戳（精确到秒）
+	Username  string `gorm:"type:varchar(20);not null"`                          //当前session对应的用户信息
+}
+
+var database, databaseERR = gorm.Open("mysql", "admin:123456@(127.0.0.1:3306)/database?charset=utf8mb4&parseTime=True&loc=Local")
+
+//连接mysql数据库
+
+func main() {
+	if databaseERR != nil {
+		panic(databaseERR)
+	}
+	defer database.Close()
+	database.SingularTable(true)
+	database.AutoMigrate(&User{})
+	database.AutoMigrate(&Salesman{})
+	database.AutoMigrate(&Repairman{})
+	database.AutoMigrate(&TypeOverview{})
+	database.AutoMigrate(&PartsOverview{})
+	database.AutoMigrate(&Vehicle{})
+	database.AutoMigrate(&Attorney{})
+	database.AutoMigrate(&Arrangement{})
+	database.AutoMigrate(&AuthSession{})
+	database.DB().SetConnMaxLifetime(time.Hour * 4) //设置超时时间
+	// 创建一个默认的路由引擎
+	r := gin.Default()
+	r.Static("/p", "./html/statics") //将相对html的路径替换成相对工程的路径
+	r.LoadHTMLFiles("./html/login.html", "./html/logout.html", "./html/register.html", "./html/error.html",
+		"./html/success.html", "./html/index.html") //加载html模板
+	r.Use(Session("SHU")) //验证码生成会使用SHU作为密钥生成session
+	// 	GET：请求方式；/index：请求的路径
+	// 	当客户端以GET方法请求/index路径时，会执行后面的匿名函数
+	//	authMiddleWare是一个中间件，用以检查cookie来判断用户是否登录。并将用户名提交给下一个中间件checkPermission。
+	//	checkPermission也是一个中间件，用以判断已登录用户所处的组别。并将用户名与组别提交给下一个匿名函数。
+	r.GET("/index", welcome)                                           //用户登录
+	r.GET("/register", startRegister)                                  //用户注册
+	r.GET("/login", startLogin)                                        //用户登录
+	r.GET("/logout", authMiddleWare(), checkPermission(), startLogout) //用户登出
+
+	r.POST("/logout", logout)                                 //后端处理用户登出
+	r.POST("/login", login)                                   //后端处理用户登陆
+	r.POST("/register", register)                             //后端处理用户注册
+	r.GET("/captcha", func(c *gin.Context) { Captcha(c, 4) }) //随机生成一个4位数字验证码
+
+	// 启动HTTP服务，在36b1c95548.qicp.vip启动服务
+	err := r.Run(":8080")
+	if err != nil {
+		fmt.Println("启动HTTP服务失败：", err)
+	}
+}
+
+func welcome(c *gin.Context) {
+	sessionId, err := c.Cookie("sessionId")
+	var username, group string
+	var session AuthSession
+	if err == nil {
+		result := database.First(&session, "time_hash=?", sessionId)
+		if result.RowsAffected == 1 { //找到了信息
+			username = session.Username
+			sTime := time.Now().Format("2006-01-02 15:04:05")
+			database.Model(&session).Update("last_visit", sTime) //用一次session，更新一次时间。
+		}
+	}
+	if strings.HasPrefix(username, "[out]") || username == "" {
+		username = "未登录"
+	}
+	var user User
+	result := database.First(&user, "contact_tel = ?", username)
+	if result.RowsAffected == 1 {
+		group = "普通用户"
+	} else {
+		var repairman Repairman
+		result := database.First(&repairman, "number = ?", username)
+		if result.RowsAffected == 1 {
+			group = "维修员"
+		} else {
+			var salesman Salesman
+			result := database.First(&salesman, "number = ?", username)
+			if result.RowsAffected == 1 {
+				group = "业务员"
+			} else {
+				group = "未登录"
+			}
+		}
+	}
+	c.HTML(http.StatusOK, "index.html", gin.H{"username": username, "group": group})
+}
+
+//-----------------------------登录与登出---------------------------------------------
+func startLogout(c *gin.Context) {
+	username := c.MustGet("username").(string)
+	group := c.MustGet("group").(string)
+	c.HTML(http.StatusOK, "logout.html", gin.H{"username": username, "group": group})
+}
+func logout(c *gin.Context) {
+	sessionId, _ := c.Cookie("sessionId")
+	var username string
+	var session AuthSession
+	result := database.First(&session, "time_hash=?", sessionId)
+	if result.RowsAffected == 1 { //找到了信息
+		username = session.Username
+		username1 := "[out]" + username
+		database.Model(&session).Update("username", username1) //在session表中将用户的账号前加入[out]标识
+		c.SetCookie("sessionId", "", 0, "", "", false, true)   //清除浏览器中的cookie
+	}
+	c.HTML(http.StatusOK, "success.html", gin.H{"data": "用户" + username + "退出登录成功！", "website": "/index", "webName": "主页"})
+}
+
+func CheckPasswordHash(password, hash string) bool { //bcrypt比对密钥函数，用以比对两个字符串中的明文是否一致
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func startRegister(c *gin.Context) {
+	sessionId, err := c.Cookie("sessionId")
+	if err == nil { //已登录
+		var session AuthSession
+		result := database.First(&session, "time_hash=?", sessionId)
+		if result.RowsAffected == 1 { //找到了信息
+			c.HTML(http.StatusForbidden, "error.html", gin.H{"errdata": "已登录，请勿在登录状态注册账号", "website": "/index", "webName": "主页"})
+			return
+		}
+	}
+	c.HTML(http.StatusOK, "register.html", nil)
+}
+
+func register(c *gin.Context) {
+	phoneNumber := c.PostForm("phone_number")
+	password := c.PostForm("password")
+	value := c.PostForm("ver_code")
+	if CaptchaVerify(c, value) { //验证码通过
+		var user User
+		number := database.Find(&user).RowsAffected + 1
+		strNumber := fmt.Sprintf("%08d", number)
+		secretPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		data := User{strNumber, string(secretPassword), "", "", 100, "", phoneNumber}
+		err := database.Create(&data)
+		strErr := fmt.Sprintf("%v", err.Error)
+		if strErr != "<nil>" {
+			c.HTML(http.StatusForbidden, "error.html", gin.H{"errdata": "注册失败！" + strErr, "website": "/register", "webName": "注册页面"})
+		} else {
+			c.HTML(http.StatusOK, "success.html", gin.H{"data": "注册成功！", "website": "/login", "webName": "登录页面"})
+		}
+	} else { //验证码错误
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"errdata": "验证码错误！", "website": "/login", "webName": "登录页面"})
+	}
+}
+
+func startLogin(c *gin.Context) {
+	sessionId, err := c.Cookie("sessionId")
+	if err == nil { //已登录
+		var session AuthSession
+		result := database.First(&session, "time_hash=?", sessionId)
+		if result.RowsAffected == 1 { //找到了信息
+			c.HTML(http.StatusForbidden, "error.html", gin.H{"errdata": "已登录，请勿重复登录", "website": "/index", "webName": "主页"})
+			return
+		}
+	}
+	c.HTML(http.StatusOK, "login.html", nil)
+}
+
+func login(c *gin.Context) {
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+	value := c.PostForm("ver_code")
+	fmt.Println(username, password)
+	if CaptchaVerify(c, value) { //验证码正确
+		var user User
+		result := database.First(&user, "contact_tel=?", username)
+		if result.RowsAffected == 1 { //只找到一条数据，用户名存在，比对密码
+			if CheckPasswordHash(password, user.Password) { //密码比对通过
+				goto CORRECT
+			} else { //密码比对不通过
+				c.HTML(http.StatusBadRequest, "error.html", gin.H{"errdata": "登录失败！", "website": "/login", "webName": "登录页面"})
+			}
+		} else { //不是用户表里的，找业务员表和维修员表
+			var repairMan Repairman
+			result := database.First(&repairMan, "number=?", username)
+			if result.RowsAffected == 1 { //只找到一条数据，用户名存在，比对密码
+				if CheckPasswordHash(password, repairMan.Password) { //密码比对通过
+					goto CORRECT
+				} else { //密码比对不通过
+					c.HTML(http.StatusBadRequest, "error.html", gin.H{"errdata": "登录失败！", "website": "/login", "webName": "登录页面"})
+				}
+			} else { //不是用户表和维修员表里的，找业务员表
+				var salesman Salesman
+				result := database.First(&salesman, "number=?", username)
+				if result.RowsAffected == 1 { //只找到一条数据，用户名存在，比对密码
+					if CheckPasswordHash(password, salesman.Password) { //密码比对通过
+						goto CORRECT
+					} else { //密码比对不通过
+						c.HTML(http.StatusBadRequest, "error.html", gin.H{"errdata": "登录失败！", "website": "/login", "webName": "登录页面"})
+					}
+				}
+			}
+		}
+	CORRECT:
+		{
+			var session []AuthSession //寻找在数据库里是否有失效的cookie（指的是用户由于关闭了浏览器，已经没有sessionId了）
+			result := database.Find(&session, "username=? or username=?", username, "[out]"+username)
+			if result.RowsAffected != 0 { //本地没有cookie，需要申请新的cookie时，删除以前保留在服务器中的cookie
+				database.Delete(&session)
+			}
+
+			curTime := time.Now()
+			nanoCurTime := curTime.UnixNano() //获得当前时间（纳秒）
+			sTime := curTime.Format("2006-01-02 15:04:05")
+			strTime := fmt.Sprintf("%d", nanoCurTime) //时间变为字符串
+			tempHash := sha256.Sum256([]byte(strTime))
+			timeHash := hex.EncodeToString(tempHash[:]) //计算时间哈希
+			data := AuthSession{timeHash, sTime, username}
+			database.Create(&data) //在authSession数据库加入一条信息
+			http.SetCookie(c.Writer, &http.Cookie{
+				Name:     "sessionId",
+				Value:    timeHash,
+				Path:     "/",
+				Domain:   "",
+				SameSite: http.SameSiteLaxMode,
+				Secure:   false,
+				HttpOnly: true,
+			})
+			c.Redirect(http.StatusMovedPermanently, "/index")
+		}
+	} else { //验证码错误
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"errdata": "验证码错误！", "website": "/login", "webName": "登录页面"})
+	}
+}
+func authMiddleWare() gin.HandlerFunc { //检查登录状态
+	return func(c *gin.Context) {
+		sessionId, err := c.Cookie("sessionId")
+		if err == nil {
+			var session AuthSession
+			result := database.First(&session, "time_hash=?", sessionId)
+			if result.RowsAffected == 1 { //找到了信息
+				sTime := time.Now().Format("2006-01-02 15:04:05")
+				database.Model(&session).Update("last_visit", sTime) //用一次session，更新一次时间。
+				c.Set("username", session.Username)
+				c.Next()
+				return
+			}
+		}
+		// 返回错误
+		c.HTML(http.StatusUnauthorized, "error.html", gin.H{"errdata": "未登录", "website": "/login", "webName": "登录页面"})
+		c.Abort()
+		return
+	}
+}
+
+func checkPermission() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		username := c.MustGet("username").(string)
+		var user User
+		result := database.First(&user, "contact_tel=?", username)
+		if result.RowsAffected == 1 {
+			c.Set("username", user.ContactTel)
+			c.Set("group", "普通用户")
+		} else {
+			var repairman Repairman
+			result := database.First(&repairman, "number=?", username)
+			if result.RowsAffected == 1 {
+				c.Set("username", repairman.Number)
+				c.Set("group", "维修员")
+			} else {
+				var salesman Salesman
+				result := database.First(&salesman, "number=?", username)
+				if result.RowsAffected == 1 {
+					c.Set("username", salesman.Number)
+					c.Set("group", "业务员")
+				} else {
+					c.HTML(http.StatusBadRequest, "error.html", gin.H{"errdata": "非法用户！", "errcode": 0, "website": "/index", "webName": "主页"})
+					c.Abort()
+				}
+			}
+		}
+	}
+}
+
+//-----------------------------验证码-------------------------------------------------------------
+func Session(keyPairs string) gin.HandlerFunc {
+	store := SessionConfig()
+	return sessions.Sessions(keyPairs, store)
+}
+func SessionConfig() sessions.Store {
+	sessionMaxAge := 600
+	sessionSecret := "SHU"
+	var store sessions.Store
+	store = cookie.NewStore([]byte(sessionSecret))
+	store.Options(sessions.Options{
+		MaxAge: sessionMaxAge, //seconds
+		Path:   "/",
+	})
+	return store
+}
+
+func Captcha(c *gin.Context, length ...int) {
+	l := captcha.DefaultLen
+	w, h := 107, 36
+	if len(length) == 1 {
+		l = length[0]
+	}
+	if len(length) == 2 {
+		w = length[1]
+	}
+	if len(length) == 3 {
+		h = length[2]
+	}
+	captchaId := captcha.NewLen(l)
+	session := sessions.Default(c)
+	session.Set("captcha", captchaId)
+	_ = session.Save()
+	_ = Serve(c.Writer, c.Request, captchaId, ".png", "zh", false, w, h)
+}
+func CaptchaVerify(c *gin.Context, code string) bool {
+	session := sessions.Default(c)
+	if captchaId := session.Get("captcha"); captchaId != nil {
+		session.Delete("captcha")
+		_ = session.Save()
+		if captcha.VerifyString(captchaId.(string), code) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+func Serve(w http.ResponseWriter, r *http.Request, id, ext, lang string, download bool, width, height int) error {
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+
+	var content bytes.Buffer
+	switch ext {
+	case ".png":
+		w.Header().Set("Content-Type", "image/png")
+		_ = captcha.WriteImage(&content, id, width, height)
+	case ".wav":
+		w.Header().Set("Content-Type", "audio/x-wav")
+		_ = captcha.WriteAudio(&content, id, lang)
+	default:
+		return captcha.ErrNotFound
+	}
+
+	if download {
+		w.Header().Set("Content-Type", "application/octet-stream")
+	}
+	http.ServeContent(w, r, id+ext, time.Time{}, bytes.NewReader(content.Bytes()))
+	return nil
+}
