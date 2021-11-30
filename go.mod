73b56c600c33248f95a243032232ebeb0bf690b6
Kachun-QLJG <1049875596@qq.com>
2021-11-30 22:19:47 +0800

    更新数据库结构、确定各类用户的功能设想

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-contrib/sessions v0.0.4
 	github.com/gin-gonic/gin v1.7.7
 	github.com/jinzhu/gorm v1.9.16
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
 )
 
 require (
@@ -26,6 +27,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -20,7 +20,7 @@ import (
 type User struct { //客户表
 	Number        string `gorm:"type:varchar(8);unique_index;not null;primary_key"` //客户编号
 	Password      string `gorm:"type:varchar(255);not null"`                        //密码
-	Name          string `gorm:"type:varchar(100)"`                                 //姓名
+	Name          string `gorm:"type:varchar(100)"`                                 //客户名称
 	Property      string `gorm:"type:varchar(4)"`                                   //性质
 	DiscountRate  int    `gorm:"type:int(2)"`                                       //折扣率
 	ContactPerson string `gorm:"type:varchar(10)"`                                  //联系人
@@ -39,28 +39,38 @@ type Repairman struct { //维修员表
 	Password        string `gorm:"type:varchar(255);not null"`                        //密码
 	Type            string `gorm:"type:varchar(10);not null"`                         //工种
 	CurrentWorkHour int    `gorm:"type:int(4);not null"`                              //当前工时
+	Status          string `gorm:"type:varchar(10);not null"`                         //工人状态
 }
 
 type TypeOverview struct { //工种总览表
-	ProjectNumber string `gorm:"type:varchar(8);not null"`   //维修项目编号
-	ProjectName   string `gorm:"type:varchar(100);not null"` //维修项目名称
-	WorkHour      string `gorm:"type:int(3);not null"`       //工时
-	Type          string `gorm:"type:varchar(10);not null"`  //工种
+	ProjectNumber string `gorm:"type:varchar(8);unique_index;not null;primary_key"` //维修项目编号
+	ProjectName   string `gorm:"type:varchar(100);not null"`                        //维修项目名称
+	Type          string `gorm:"type:varchar(10);not null;primary_key"`             //工种
 }
 
 type PartsOverview struct { //零件总览表
 	PartsNumber string  `gorm:"type:varchar(8);unique_index;not null;primary_key"` //零件编号
 	PartsName   string  `gorm:"type:varchar(50);not null"`                         //零件名称
+	Unit        string  `gorm:"type:varchar(6);not null"`                          //计量单位
 	PartsCost   float64 `gorm:"type:double(8,2);not null"`                         //零件价格
 }
 
 type Vehicle struct { //车辆表
-	Number        string `gorm:"type:varchar(17);not null;unique_index;primary_key"` //车架号
-	LicenseNumber string `gorm:"type:varchar(10);not null"`                          //最后一次访问的时间戳（精确到秒）
-	UserID        string `gorm:"type:varchar(8);not null"`                           //客户编号
-	Color         string `gorm:"type:varchar(10);not null"`                          //车辆颜色
-	Model         string `gorm:"type:varchar(40);not null"`                          //车型
-	Type          string `gorm:"type:varchar(10);not null"`                          //车型
+	Number        string `gorm:"type:varchar(17);not null;primary_key"` //车架号
+	LicenseNumber string `gorm:"type:varchar(10);not null"`             //车牌号
+	UserID        string `gorm:"type:varchar(8);not null;primary_key"`  //客户编号
+	Color         string `gorm:"type:varchar(10);not null"`             //车辆颜色
+	Model         string `gorm:"type:varchar(40);not null"`             //车型
+	Type          string `gorm:"type:varchar(10);not null"`             //车辆类别
+	Time          string `gorm:"type:varchar(20);not null"`             //绑定时间
+}
+
+type Notification struct { //通知表
+	Number  string `gorm:"type:varchar(8);unique_index;not null;primary_key"` //通知编号
+	UserID  string `gorm:"type:varchar(8);not null"`                          //用户号
+	Title   string `gorm:"type:varchar(50);not null"`                         //通知标题
+	Content string `gorm:"type:varchar(255);not null"`                        //通知内容
+	Status  string `gorm:"type:varchar(4);not null"`                          //接收状态
 }
 
 type Attorney struct { //委托书表
@@ -78,15 +88,22 @@ type Attorney struct { //委托书表
 	SpecificProblem   string  `gorm:"type:varchar(255);not null"`                         //详细故障描述
 	Progress          string  `gorm:"type:varchar(10);not null"`                          //进展
 	TotalCost         float64 `gorm:"type:double(6,2);not null"`                          //总价
+	StartPetrol       float64 `gorm:"type:double(5,2);not null"`                          //进厂油量
+	StartMile         float64 `gorm:"type:double(8,2);not null"`                          //进厂里程
+	EndPetrol         float64 `gorm:"type:double(5,2)"`                                   //出厂油量
+	EndMile           float64 `gorm:"type:double(8,2)"`                                   //出厂里程
+	OutRange          string  `gorm:"type:varchar(255)"`                                  //非维修范围
 }
 
 type Arrangement struct { //派工单表
-	OrderNumber     string `gorm:"type:varchar(11);not null"` //订单号
-	ProjectNumber   string `gorm:"type:varchar(8);not null"`  //维修项目编号
-	RepairmanNumber string `gorm:"type:varchar(8);not null"`  //订单号
-	PartsNumber     string `gorm:"type:varchar(8);not null"`  //零件号
-	PartsCount      int    `gorm:"type:int(2);not null"`      //零件数量
-	Progress        string `gorm:"type:varchar(6);not null"`  //进展
+	OrderNumber     string `gorm:"type:varchar(11);not null;primary_key"` //订单号
+	ProjectNumber   string `gorm:"type:varchar(8);not null;primary_key"`  //维修项目编号
+	PredictTime     int    `gorm:"type:int(3);not null"`                  //预计工时
+	ActualTIme      int    `gorm:"type:int(3);not null"`                  //实际工时
+	RepairmanNumber string `gorm:"type:varchar(8);not null;primary_key"`  //维修工工号
+	PartsNumber     string `gorm:"type:varchar(8);not null"`              //零件号
+	PartsCount      int    `gorm:"type:int(2);not null"`                  //零件数量
+	Progress        string `gorm:"type:varchar(6);not null"`              //进展
 }
 
 type AuthSession struct { //登录表
@@ -115,6 +132,7 @@ func main() {
 	database.AutoMigrate(&Attorney{})
 	database.AutoMigrate(&Arrangement{})
 	database.AutoMigrate(&AuthSession{})
+	database.AutoMigrate(&Notification{})
 
 	database.Model(&Vehicle{}).AddForeignKey("user_id", "user(number)", "RESTRICT", "RESTRICT")
 	database.Model(&Attorney{}).AddForeignKey("user_id", "user(number)", "RESTRICT", "RESTRICT")
@@ -122,7 +140,11 @@ func main() {
 	database.Model(&Attorney{}).AddForeignKey("salesman_id", "salesman(number)", "RESTRICT", "RESTRICT")
 	database.Model(&Arrangement{}).AddForeignKey("order_number", "attorney(number)", "RESTRICT", "RESTRICT")
 	database.Model(&Arrangement{}).AddForeignKey("repairman_number", "repairman(number)", "RESTRICT", "RESTRICT")
+	database.Model(&Arrangement{}).AddForeignKey("project_number", "type_overview(project_number)", "RESTRICT", "RESTRICT")
 	database.Model(&Arrangement{}).AddForeignKey("parts_number", "parts_overview(parts_number)", "RESTRICT", "RESTRICT")
+	database.Model(&Notification{}).AddForeignKey("user_id", "user(number)", "RESTRICT", "RESTRICT")
+	database.Model(&Notification{}).AddForeignKey("user_id", "repairman(number)", "RESTRICT", "RESTRICT")
+	database.Model(&Notification{}).AddForeignKey("user_id", "salesman(number)", "RESTRICT", "RESTRICT")
 
 	database.DB().SetConnMaxLifetime(time.Hour * 4) //设置超时时间
 	// 创建一个默认的路由引擎
@@ -160,7 +182,8 @@ func addVehicle(c *gin.Context) {
 	color := c.Query("color")
 	model := c.Query("model")
 	carType := c.Query("type")
-	data := Vehicle{number, licenseNumber, userId, color, model, carType}
+	sTime := time.Now().Format("2006-01-02 15:04:05")
+	data := Vehicle{number, licenseNumber, userId, color, model, carType, sTime}
 	err := database.Create(&data)
 	strErr := fmt.Sprintf("%v", err.Error)
 	if strErr != "<nil>" {
