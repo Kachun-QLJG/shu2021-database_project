e3764db50fe74c686570ee2c7e06db3ae2c6cd7b
Kachun-QLJG <1049875596@qq.com>
2021-12-02 19:19:07 +0800

    消息功能、修改密码功能已加入，axios已导入。目前只设置了index具有消息功能。

--- server/server.go
+++ server/server.go
@@ -142,36 +142,117 @@ func main() {
 	database.Model(&Arrangement{}).AddForeignKey("repairman_number", "repairman(number)", "RESTRICT", "RESTRICT")
 	database.Model(&Arrangement{}).AddForeignKey("project_number", "type_overview(project_number)", "RESTRICT", "RESTRICT")
 	database.Model(&Arrangement{}).AddForeignKey("parts_number", "parts_overview(parts_number)", "RESTRICT", "RESTRICT")
-	database.Model(&Notification{}).AddForeignKey("user_id", "user(number)", "RESTRICT", "RESTRICT")
-	database.Model(&Notification{}).AddForeignKey("user_id", "repairman(number)", "RESTRICT", "RESTRICT")
-	database.Model(&Notification{}).AddForeignKey("user_id", "salesman(number)", "RESTRICT", "RESTRICT")
+	//database.Model(&Notification{}).AddForeignKey("user_id", "user(number)", "RESTRICT", "RESTRICT")
+	//database.Model(&Notification{}).AddForeignKey("user_id", "repairman(number)", "RESTRICT", "RESTRICT")
+	//database.Model(&Notification{}).AddForeignKey("user_id", "salesman(number)", "RESTRICT", "RESTRICT")
 
-	database.DB().SetConnMaxLifetime(time.Hour * 4) //设置超时时间
+	database.DB().SetConnMaxLifetime(time.Hour * 24 * 21) //设置超时时间
 	// 创建一个默认的路由引擎
 	r := gin.Default()
 	r.Static("/p", "./html/statics") //将相对html的路径替换成相对工程的路径
 	r.LoadHTMLFiles("./html/login.html", "./html/logout.html", "./html/register.html", "./html/error.html",
-		"./html/success.html", "./html/index.html") //加载html模板
+		"./html/success.html", "./html/index.html", "./html/change_password.html") //加载html模板
 	r.Use(Session("SHU")) //验证码生成会使用SHU作为密钥生成session
 	// 	GET：请求方式；/index：请求的路径
 	// 	当客户端以GET方法请求/index路径时，会执行后面的匿名函数
 	//	authMiddleWare是一个中间件，用以检查cookie来判断用户是否登录。并将用户名提交给下一个中间件checkPermission。
 	//	checkPermission也是一个中间件，用以判断已登录用户所处的组别。并将用户名与组别提交给下一个匿名函数。
-	r.GET("/index", welcome)                                           //用户登录
-	r.GET("/register", startRegister)                                  //用户注册
-	r.GET("/login", startLogin)                                        //用户登录
-	r.GET("/logout", authMiddleWare(), checkPermission(), startLogout) //用户登出
-	r.GET("/addVehicle", addVehicle)                                   //用户登出
+	r.GET("/index", welcome)                                                            //用户登录
+	r.GET("/register", startRegister)                                                   //用户注册
+	r.GET("/login", startLogin)                                                         //用户登录
+	r.GET("/logout", authMiddleWare(), checkPermission(), startLogout)                  //用户登出
+	r.GET("/addVehicle", addVehicle)                                                    //用户登出
+	r.GET("/changePassword", authMiddleWare(), checkPermission(), startChangePassword)  //更改密码
+	r.GET("/checkNotification", authMiddleWare(), checkPermission(), checkNotification) //检查通知
+	r.GET("/read", authMiddleWare(), checkPermission(), read)                           //设置已读
 
-	r.POST("/logout", logout)                                 //后端处理用户登出
-	r.POST("/login", login)                                   //后端处理用户登陆
-	r.POST("/register", register)                             //后端处理用户注册
-	r.GET("/captcha", func(c *gin.Context) { Captcha(c, 4) }) //随机生成一个4位数字验证码
+	r.POST("/changePassword", authMiddleWare(), checkPermission(), changePassword) //后端处理更改密码
+	r.POST("/logout", logout)                                                      //后端处理用户登出
+	r.POST("/login", login)                                                        //后端处理用户登陆
+	r.POST("/register", register)                                                  //后端处理用户注册
+	r.GET("/captcha", func(c *gin.Context) { Captcha(c, 4) })                      //随机生成一个4位数字验证码
 
 	// 启动HTTP服务，在36b1c95548.qicp.vip启动服务
 	err := r.Run(":8080")
 	if err != nil {
 		fmt.Println("启动HTTP服务失败：", err)
+	}
+}
+
+func read(c *gin.Context) {
+	username := c.MustGet("username").(string)
+	var notification Notification
+	database.First(&notification, "user_id = ? and status = ?", username, "未读")
+	database.Model(&notification).Update("status", "已读") //更改消息为已读
+}
+
+func checkNotification(c *gin.Context) {
+	username := c.MustGet("username").(string)
+	var notification Notification
+	result := database.First(&notification, "user_id = ? and status = ?", username, "未读")
+	if result.RowsAffected == 1 {
+		c.JSON(http.StatusOK, gin.H{"title": notification.Title, "content": notification.Content})
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+}
+
+func startChangePassword(c *gin.Context) {
+	username := c.MustGet("username").(string)
+	group := c.MustGet("group").(string)
+	c.HTML(http.StatusOK, "change_password.html", gin.H{"username": username, "group": group})
+}
+
+func changePassword(c *gin.Context) {
+	oldPassword := c.PostForm("old_password")
+	newPassword := c.PostForm("new_password")
+	username := c.MustGet("username").(string)
+	group := c.MustGet("group").(string)
+	if group == "普通用户" {
+		var user User
+		database.First(&user, "contact_tel = ?", username)
+		if CheckPasswordHash(oldPassword, user.Password) { //密码比对通过
+			secretPassword, _ := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+			database.Model(&user).Update("password", secretPassword) //用一次session，更新一次时间。
+			username1 := "[out]" + username
+			var session AuthSession
+			database.First(&session, "username = ?", username)
+			database.Model(&session).Update("username", username1) //在session表中将用户的账号前加入[out]标识
+			c.SetCookie("sessionId", "", 0, "", "", false, true)   //清除浏览器中的cookie
+			c.HTML(http.StatusOK, "success.html", gin.H{"data": "密码更改成功！", "website": "/login", "webName": "登录页面"})
+		} else {
+			c.HTML(http.StatusOK, "error.html", gin.H{"data": "密码错误！", "website": "/change_password", "webName": "修改密码页面"})
+		}
+	} else if group == "业务员" {
+		var user Salesman
+		database.First(&user, "number = ?", username)
+		if CheckPasswordHash(oldPassword, user.Password) { //密码比对通过
+			secretPassword, _ := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+			database.Model(&user).Update("password", secretPassword) //用一次session，更新一次时间。
+			username1 := "[out]" + username
+			var session AuthSession
+			database.First(&session, "username = ?", username)
+			database.Model(&session).Update("username", username1) //在session表中将用户的账号前加入[out]标识
+			c.SetCookie("sessionId", "", 0, "", "", false, true)   //清除浏览器中的cookie
+			c.HTML(http.StatusOK, "success.html", gin.H{"data": "密码更改成功！", "website": "/login", "webName": "登录页面"})
+		} else {
+			c.HTML(http.StatusOK, "error.html", gin.H{"data": "密码错误！", "website": "/change_password", "webName": "修改密码页面"})
+		}
+	} else {
+		var user Repairman
+		database.First(&user, "number = ?", username)
+		if CheckPasswordHash(oldPassword, user.Password) { //密码比对通过
+			secretPassword, _ := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+			database.Model(&user).Update("password", secretPassword) //用一次session，更新一次时间。
+			username1 := "[out]" + username
+			var session AuthSession
+			database.First(&session, "username = ?", username)
+			database.Model(&session).Update("username", username1) //在session表中将用户的账号前加入[out]标识
+			c.SetCookie("sessionId", "", 0, "", "", false, true)   //清除浏览器中的cookie
+			c.HTML(http.StatusOK, "success.html", gin.H{"data": "密码更改成功！", "website": "/login", "webName": "登录页面"})
+		} else {
+			c.HTML(http.StatusOK, "error.html", gin.H{"data": "密码错误！", "website": "/change_password", "webName": "修改密码页面"})
+		}
 	}
 }
 
