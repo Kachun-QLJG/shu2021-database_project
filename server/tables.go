6e734471aa3ffb7a43c4525dfbd02f1e44d3f7c5
f8ing <f8ingX@163.com>
2021-12-30 15:18:24 +0800

    新增维修委托模态窗

--- server/tables.go
+++ server/tables.go
@@ -66,19 +66,19 @@ type Notification struct { //通知表
 type Attorney struct { //委托书表
 	Number            string  `gorm:"type:varchar(11);not null;unique_index;primary_key"` //订单号
 	UserID            string  `gorm:"type:varchar(8);not null"`                           //客户编号
-	VehicleNumber     string  `gorm:"type:varchar(17);not null"`                          //车架号
+	VehicleNumber     string  `gorm:"type:varchar(17);not null"`                          //车架号（要填）
 	RepairType        string  `gorm:"type:varchar(4)"`                                    //维修类型
 	Classification    string  `gorm:"type:varchar(4)"`                                    //作业分类
-	PayMethod         string  `gorm:"type:varchar(4)"`                                    //结算方式
-	StartTime         string  `gorm:"type:varchar(20);not null"`                          //进场时间
+	PayMethod         string  `gorm:"type:varchar(4);not null"`                           //结算方式（要填）
+	StartTime         string  `gorm:"type:varchar(20);not null"`                          //进场时间（要填）
 	SalesmanID        string  `gorm:"type:varchar(8)"`                                    //业务员编号
 	PredictFinishTime string  `gorm:"type:varchar(20)"`                                   //预计完工时间时间
 	ActualFinishTime  string  `gorm:"type:varchar(20)"`                                   //实际完工时间时间
-	RoughProblem      string  `gorm:"type:varchar(255);not null"`                         //粗略故障描述
-	SpecificProblem   string  `gorm:"type:varchar(255);not null"`                         //详细故障描述
+	RoughProblem      string  `gorm:"type:varchar(255);not null"`                         //粗略故障描述（要填）
+	SpecificProblem   string  `gorm:"type:varchar(255);"`                                 //详细故障描述
 	Progress          string  `gorm:"type:varchar(10);not null"`                          //进展
-	StartPetrol       float64 `gorm:"type:double(5,2);not null"`                          //进厂油量
-	StartMile         float64 `gorm:"type:double(8,2);not null"`                          //进厂里程
+	StartPetrol       float64 `gorm:"type:double(5,2);not null"`                          //进厂油量（要填）
+	StartMile         float64 `gorm:"type:double(8,2);not null"`                          //进厂里程（要填）
 	EndPetrol         float64 `gorm:"type:double(5,2)"`                                   //出厂油量
 	EndMile           float64 `gorm:"type:double(8,2)"`                                   //出厂里程
 	OutRange          string  `gorm:"type:varchar(255)"`                                  //非维修范围
